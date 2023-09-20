680147d806f498521bee6526d1e7419ff3e1c4c3
Gourav <gourav@g.com>
2023-09-20 09:32:14 +0530

    Integral data type

--- src/datatypes/IntegerType.java
+++ src/datatypes/IntegerType.java
@@ -18,6 +18,23 @@ public class IntegerType {
 		
 		System.out.println(var2);
 		
+		
+		int i = 2;
+		
+		int j = 128;
+		
+		int k = 32767;
+		
+		int l = 2147483647;
+		
+		int m = -2147483648;
+		
+		System.out.println(l);
+		
+		long u = 45l;
+		
+		System.out.println(u);
+		
 
 	}
 	
