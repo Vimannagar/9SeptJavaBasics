0d46acef9073483f43c9e0cb87fbd2b9dfc2b8c5
Gourav <gourav@g.com>
2023-09-22 10:29:21 +0530

    Operators

--- src/datatypes/Addition.java
+++ src/datatypes/Addition.java
@@ -11,12 +11,41 @@ public class Addition {
 		
 		int k = i+j;
 		
-		System.out.println(k);
+		System.out.println(k);//30
 		
 		
 //		Assignment - Write a Program to perform substraction and multiplication in java
 		
+		// substraction
 		
+		int l = 60;
+		
+		int m = 10;
+		
+		int n = l-m;
+		
+		System.out.println(n);//50
+		
+		
+//		Multiplication
+		
+		int z = 20;
+		
+		int y = 80;
+		
+		int x = z*y;
+		
+		System.out.println(x);//1600
+		
+//		Division
+		
+		double e = 100000000;
+		
+		double f = 565656;
+		
+		double g = e/f;
+		
+		System.out.println(g);//3.333333335
 		
 	}
 
---
+++ src/datatypes/NonPrimitive.java
@@ -0,0 +1,34 @@
+package datatypes;
+
+public class NonPrimitive {
+	
+	
+	public static void main(String[] args) {
+		// datatype variablename = value;
+		
+		String s = "this is string value";
+		
+		System.out.println(s);//this is string value
+		
+		
+// 1. Concatenation +
+		
+		String s1 = "Pune";
+		
+		String s2 = "Mahanagar";
+		
+		String s3 = s1+s2;
+		
+		System.out.println(s3);//PuneMahanagar
+		
+		
+		String s4 = s1+" "+s2;
+		
+		System.out.println(s4);
+		
+		
+		
+		
+	}
+
+}
