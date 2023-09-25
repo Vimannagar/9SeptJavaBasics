46ce775919d728be39fd157d1aaef8b528bd99af
Gourav <gourav@g.com>
2023-09-25 09:30:22 +0530

    concatenation rules

--- src/datatypes/NonPrimitive.java
+++ src/datatypes/NonPrimitive.java
@@ -24,8 +24,37 @@ public class NonPrimitive {
 		
 		String s4 = s1+" "+s2;
 		
-		System.out.println(s4);
+		System.out.println(s4);//Pune Mahanagar
 		
+		
+		String s5 = "Mumbai";
+		
+		String s6 = "India's financial capital is ";
+		
+		String s7 = s6+s5;
+		
+		System.out.println(s7);//India's financial capital is Mumbai
+		
+		System.out.println(s6+s5);//India's financial capital is Mumbai
+		
+		int a = 10;
+		
+		int b = 20;
+		
+		System.out.println(a+b);//30
+		
+		
+//		String with numbers:
+		
+		int z = 100;
+		
+		int y = 20;
+		
+		int e = z+y;
+		
+		System.out.println(e);//70
+		
+		System.out.println("The addition of "+z+" and "+y+" is "+e);// The addition of c and d is 70
 		
 		
 		
