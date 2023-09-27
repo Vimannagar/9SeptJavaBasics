46a895bc2812d15f6f594a5d2a670f02749bb515
Gourav <gourav@g.com>
2023-09-27 09:30:47 +0530

    static method

--- src/method/StaticMethod.java
+++ src/method/StaticMethod.java
@@ -2,10 +2,58 @@ package method;
 
 public class StaticMethod {
 	
-	public static void main(String[] args) {
-		
-
+	
+//	Syntax:
+//	public static void method_name()
+//	{
+//		logic which we want to run	
+//	}
+	
+	
+	public static void methodOne()
+	{		
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
 		
 	}
+	
+	public static void addition()
+	{
+		int a = 10;
+		
+		int b = 50;
+		
+		int c =  a+b;
+		
+		System.out.println(c);
+	}
+	
+	public static void main(String[] args) {
+		
+//		calling the static method inside the main method-- > directly methodname();
+	
+		addition();
+		
+		methodOne();
+		
+		methodOne();
+		
+		methodOne();
+		addition();
+		
+//		Calling static method using second way-> classname.methodname();
+		
+		StaticMethod.addition();
+		
+		
+		
+	}
+	
+	
+	
+	
 
 }
---
+++ src/method/Test.java
@@ -0,0 +1,16 @@
+package method;
+
+public class Test {
+	
+
+	
+	public static void main(String[] args) {
+		
+		StaticMethod.methodOne();
+		
+		StaticMethod.addition();
+		
+		
+	}
+
+}
